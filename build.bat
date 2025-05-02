e2561875b44b6edee02ecf9b9b317be9ab8f55dd
Kinugawa Akihito <akngw@users.noreply.github.com>
2025-05-02 21:29:25 +0900

    ビルド用バッチファイルをVS2022 Community向けに変更する

--- build.bat
+++ build.bat
@@ -2,11 +2,18 @@
 setlocal
 cd /d %~dp0
 
-call "%VS120COMNTOOLS%\vsvars32.bat"
+set "VCVARSALL_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
+
+if not exist "%VCVARSALL_PATH%" (
+    echo Error: "%VCVARSALL_PATH%" not found.
+    exit /b 1
+)
+
+call "%VCVARSALL_PATH%" x86
 
 if "%1"=="" (set CONFIGURATION=Release) else (set CONFIGURATION=%1)
 if "%2"=="" (set TARGET=Build) else (set TARGET=%2)
 if "%3"=="" (set VERBOSITY=normal) else (set VERBOSITY=%3)
 
 echo Build xyzzy...
-msbuild xyzzy.sln /nologo /p:Configuration=%CONFIGURATION% /target:%TARGET% /verbosity:%VERBOSITY% %4 %5 %6 %7 %8 %9 || exit /b 1
+msbuild xyzzy.sln /nologo /p:Configuration=%CONFIGURATION% /p:Platform=Win32 /target:%TARGET% /verbosity:%VERBOSITY% %4 %5 %6 %7 %8 %9 || exit /b 1
